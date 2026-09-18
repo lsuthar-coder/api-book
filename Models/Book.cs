11f0fd3e568dfbc8526aeaca967a6cc77e6b2dba
Leeladhar suthar <leeladharsuthar80@gmail.com>
2026-09-18 19:42:25 +0530

    18-09-2026(v2)

--- Models/Book.cs
+++ Models/Book.cs
@@ -6,13 +6,18 @@ namespace BookStoreApi.Models;
 
 public class Book
 {
+    // Marks this property as the primary key (_id) in the MongoDB collection
     [BsonId]
+    // Converts MongoDB's binary ObjectId into a clean C# string automatically
     [BsonRepresentation(BsonType.ObjectId)]
     public string? Id { get; set; }
 
+    // Maps the C# 'BookName' property to a database field named "Name" in MongoDB
     [BsonElement("Name")]
+    // Forces the Web API to use exactly "Name" (PascalCase) in the network JSON payload
     [JsonPropertyName("Name")]
     public string BookName { get; set; } = null!;
+
 
     public decimal Price { get; set; }
 
